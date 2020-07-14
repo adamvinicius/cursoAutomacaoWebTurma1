9db6470e3cf8b356650ca0b7ba06c034b00630d3
Adam Vinicius <adamcursogit@gmail.com>
2020-07-14 09:19:21 -0300

    list elements

--- src/test/java/core/Element.java
+++ src/test/java/core/Element.java
@@ -1,13 +1,21 @@
 package core;
 
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 
 import enums.ByValue;
 
 public class Element {
 	ByValue by;
 	String map;
+	WebElement webElement = null;
+	
+	public void setWebElement(WebElement _element) {
+		webElement = _element;
+	}
 	
 	public Element(ByValue by, String _map) {
 		this.by = by;
@@ -18,24 +26,56 @@ public class Element {
 		WebElement element = null;
 		switch (by) {
 		case ID:
-			element = Driver.getDriver().findElement(By.id(map));
+			element = get(By.id(map));		
 			break;
 		case XPATH:
-			element = Driver.getDriver().findElement(By.xpath(map));
+			element = get(By.xpath(map));
 			break;
 		case CSS:
-			element = Driver.getDriver().findElement(By.cssSelector(map));
+			element = get(By.cssSelector(map));
 			break;
 		case LINKTEXT:
-			element = Driver.getDriver().findElement(By.linkText(map));
+			element = get(By.linkText(map));
 			break;
 		case NAME:
-			element = Driver.getDriver().findElement(By.name(map));
+			element = get(By.name(map));
 			break;
 		default:
 			break;
 		}
 		return element;
+	}
+	
+	public WebElement get(By by) {
+		if (webElement == null) {
+			return Driver.getDriver().findElement(by);
+		}else {
+			return webElement.findElement(by);
+		}
+	}
+			
+	public List<WebElement> getElements() {
+		List<WebElement> elements = null;
+		switch (by) {
+		case ID:
+			elements = Driver.getDriver().findElements(By.id(map));
+			break;
+		case XPATH:
+			elements = Driver.getDriver().findElements(By.xpath(map));
+			break;
+		case CSS:
+			elements = Driver.getDriver().findElements(By.cssSelector(map));
+			break;
+		case LINKTEXT:
+			elements = Driver.getDriver().findElements(By.linkText(map));
+			break;
+		case NAME:
+			elements = Driver.getDriver().findElements(By.name(map));
+			break;
+		default:
+			break;
+		}
+		return elements;
 	}
 	
 	public void sendKeys(CharSequence... value) {
@@ -48,5 +88,30 @@ public class Element {
 	
 	public void click() {
 		getElement().click();
+	}
+	
+	public String getAttribute(String value) {
+		return getElement().getAttribute(value);
+	}
+	
+	public void clear() {
+		getElement().clear();
+	}
+	
+	public boolean isEnabled() {
+		return getElement().isEnabled();
+	}
+	
+	public boolean isDisplayed() {
+		return getElement().isDisplayed();
+	}
+	
+	public boolean isSelected() {
+		return getElement().isSelected();
+	}
+	
+	public void select(String value) {
+		Select select = new Select(getElement());
+		select.selectByVisibleText(value);
 	}
 }
--- src/test/java/pages/InicialPage.java
+++ src/test/java/pages/InicialPage.java
@@ -32,13 +32,13 @@ public class InicialPage {
 	}
 	
 	public String getPrecoLista() {
-		List<WebElement> livros = Driver.getDriver().findElements(By.cssSelector(elLivros));
+		List<WebElement> livros = inicialMap.livros.getElements();
 		for (WebElement elLivro : livros) {
-			WebElement tituloLivro = elLivro.findElement(By.cssSelector(elTituloLivroLista));
-			String titulo = tituloLivro.getText();
+			inicialMap.tituloLivroLista.setWebElement(elLivro);
+			String titulo = inicialMap.tituloLivroLista.getText();
 			if (titulo.contains("Ajax com Java")) {
-				WebElement preco = elLivro.findElement(By.cssSelector(elPrecoLista));
-				return preco.getText();
+				inicialMap.precoLista.setWebElement(elLivro);
+				return inicialMap.precoLista.getText();
 			}
 		}
 		return null;
